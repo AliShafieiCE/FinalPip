0baa700af422b70d85b87ac9a1bce90ce6ad568e
AliShelby <51177681+AliShelby@users.noreply.github.com>
2019-07-08 02:51:22 -0700

    Add files via upload

---
+++ WorkingPip/PipDemo/demo.py
@@ -0,0 +1,13 @@
+import os
+import tensorflow as tf
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def printRes():
+	file1 = open("file1.txt", "r")
+
+	sess = tf.compat.v1.Session()
+	a = tf.constant(int(file1.readline(), 10))
+	b = tf.constant(int(file1.readline(), 10))
+	print(sess.run(a + b))
